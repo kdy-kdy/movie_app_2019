b1fa52d7e5d8104bd1059fbe413431d27afedd36
kdy-kyd <daye109@naver.com>
2019-09-19 20:46:18 +0900

    Updates

---
+++ precache-manifest.e68efea83929f9b53878487b72937cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da49e744ba543c1ebb98fa50bd20880c",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "25a03cc3afbad0147b69",
+    "url": "/movie_app_2019/static/css/main.399affe9.chunk.css"
+  },
+  {
+    "revision": "bdd8a808aa278518fafc",
+    "url": "/movie_app_2019/static/js/2.86a49d24.chunk.js"
+  },
+  {
+    "revision": "25a03cc3afbad0147b69",
+    "url": "/movie_app_2019/static/js/main.ee831658.chunk.js"
+  },
+  {
+    "revision": "29aaad419714eb018816",
+    "url": "/movie_app_2019/static/js/runtime~main.f4904fd8.js"
+  }
+]);
